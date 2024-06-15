b60e49a11afae743ab68578627ec1636928c7b5a
Everaldo Matias <matiaseveraldo@gmail.com>
2024-06-15 19:56:43 -0300

    Remove activation plugins

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ if ! wp core is-installed --allow-root; then
     echo "Instalando o WordPress..."
     wp core install --url=${WORDPRESS_URL} --title="${WORDPRESS_TITLE}" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
     wp plugin install wp-data-logger query-monitor jsm-show-post-meta --activate --allow-root
-    wp plugin install wordfence pods contact-form-7 --activate --allow-root
+    wp plugin install wordfence pods contact-form-7 --allow-root
 fi
 
 wp rewrite structure '/%postname%/' --hard --allow-root
